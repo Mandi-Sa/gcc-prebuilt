701a48ed471b54360ba4fd2e1aec5c6eb534081e
阿菌•未霜 <799620521@qq.com>
2021-10-18 00:27:30 +0800

    GCC 12.0.0 with binutils 2.37 20211017 prebuilt for amd64.

---
+++ lib/gcc/arm-none-eabi/12.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/develop/gcc/source/gcc/configure --prefix=/develop/gcc/install --target=arm-none-eabi --with-pkgversion=阿菌•未霜 --disable-shared --with-newlib --with-gnu-as --with-gnu-ld --enable-lto --enable-linker-build-id --disable-libmudflap --disable-libgomp --disable-libssp --disable-libstdcxx-pch --enable-multiarch --disable-multilib --enable-languages=c,c++ --with-headers=/develop/gcc/source/newlib-cygwin/newlib/libc/include --with-mpc=/develop/gcc/build/buildtools --with-isl=/develop/gcc/build/buildtools --with-mpfr=/develop/gcc/build/buildtools --with-gmp=/develop/gcc/build/buildtools CFLAGS='-s -O3 -flto -ffat-lto-objects -fuse-linker-plugin' CXXFLAGS='-s -O3 -flto -ffat-lto-objects -fuse-linker-plugin'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
