405fbcefe03fc66ecab937dc27edf107348e25a3
阿菌•未霜 <799620521@qq.com>
2022-04-23 20:52:33 +0800

    GCC 12.0.1 with binutils 2.38.50 20220423 prebuilt for amd64.

--- aarch64-elf/lib/libstdc++.a-gdb.py
+++ aarch64-elf/lib/libstdc++.a-gdb.py
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/develop/gcc/install/share/gcc-12.0.0/python'
+pythondir = '/develop/gcc/install/share/gcc-12.0.1/python'
 libdir = '/develop/gcc/install/aarch64-elf/lib'
 
 # This file might be loaded when there is no current objfile.  This
