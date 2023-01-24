da55d9c2ce93c143ae8177eb11d248dc82b45f19
阿菌•未霜 <799620521@qq.com>
2023-01-24 22:11:40 +0800

    GCC 13.0.1 with binutils 2.40.50 20230124 prebuilt for amd64.

--- aarch64-elf/lib/libstdc++.a-gdb.py
+++ aarch64-elf/lib/libstdc++.a-gdb.py
@@ -1,5 +1,5 @@
 # -*- python -*-
-# Copyright (C) 2009-2022 Free Software Foundation, Inc.
+# Copyright (C) 2009-2023 Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/develop/gcc/install/share/gcc-13.0.0/python'
+pythondir = '/develop/gcc/install/share/gcc-13.0.1/python'
 libdir = '/develop/gcc/install/aarch64-elf/lib'
 
 # This file might be loaded when there is no current objfile.  This
