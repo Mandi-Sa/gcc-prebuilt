c4f22ab543afe0eb952d3e17d3b363ef41b6e7ce
阿菌•未霜 <799620521@qq.com>
2023-05-23 20:47:15 +0800

    GCC 14.0.0 with binutils 2.40 20230523 prebuilt for amd64.

---
+++ lib/gcc/arm-eabi/14.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/develop/toolchain/crosstools/arm/.build/arm-eabi/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=arm-eabi --prefix=/root/x-tools/arm-eabi --exec_prefix=/root/x-tools/arm-eabi --with-local-prefix=/root/x-tools/arm-eabi/arm-eabi --with-headers=/root/x-tools/arm-eabi/arm-eabi/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion=阿菌•未霜 --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/develop/toolchain/crosstools/arm/.build/arm-eabi/buildtools --with-mpfr=/develop/toolchain/crosstools/arm/.build/arm-eabi/buildtools --with-mpc=/develop/toolchain/crosstools/arm/.build/arm-eabi/buildtools --with-isl=/develop/toolchain/crosstools/arm/.build/arm-eabi/buildtools --enable-lto --with-long-double-128 --disable-nls --with-system-zlib --enable-tls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
