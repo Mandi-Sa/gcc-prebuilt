f381775ab6bbe8629b0d687e9a283a0ef60c611f
阿菌•未霜 <799620521@qq.com>
2023-05-23 22:43:16 +0800

    GCC 14.0.0 with binutils 2.40 20230523 prebuilt for amd64.

---
+++ lib/gcc/x86_64-elf/14.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/develop/toolchain/crosstools/x86_64/.build/x86_64-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=x86_64-elf --prefix=/root/x-tools/x86_64-elf --exec_prefix=/root/x-tools/x86_64-elf --with-local-prefix=/root/x-tools/x86_64-elf/x86_64-elf --with-headers=/root/x-tools/x86_64-elf/x86_64-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion=阿菌•未霜 --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/develop/toolchain/crosstools/x86_64/.build/x86_64-elf/buildtools --with-mpfr=/develop/toolchain/crosstools/x86_64/.build/x86_64-elf/buildtools --with-mpc=/develop/toolchain/crosstools/x86_64/.build/x86_64-elf/buildtools --with-isl=/develop/toolchain/crosstools/x86_64/.build/x86_64-elf/buildtools --enable-lto --with-long-double-128 --disable-nls --with-system-zlib --enable-tls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
